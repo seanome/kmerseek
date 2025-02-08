cb7858c65778ec620123820471f84f3c8ea572dd
Olga Botvinnik <olga.botvinnik@gmail.com>
2025-02-08 01:27:12 +0000

    "index" now outputs a parquet file

--- tests/test_index.py
+++ tests/test_index.py
@@ -14,6 +14,6 @@ def test_index(bcl2_first25):
     # Make sure all the files got created
     assert os.path.exists(f"{bcl2_first25}.manysketch.csv")
     assert os.path.exists(f"{bcl2_first25}.hp.k24.scaled5.sig.zip")
-    assert os.path.exists(f"{bcl2_first25}.hp.k24.scaled5.sig.zip.kmers.csv")
+    assert os.path.exists(f"{bcl2_first25}.hp.k24.scaled5.sig.zip.kmers.pq")
     assert os.path.exists(f"{bcl2_first25}.hp.k24.scaled5.sig.zip.siglist")
     assert os.path.exists(f"{bcl2_first25}.hp.k24.scaled5.sig.zip.rocksdb")
