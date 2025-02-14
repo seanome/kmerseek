344df4bb5fa1e3c75353c143a7ecd971a31e8dd9
Olga Botvinnik <olga.botvinnik@gmail.com>
2025-02-14 09:00:29 -0800

    Add rocksdb checking, black formatting

--- tests/test_index.py
+++ tests/test_index.py
@@ -36,3 +36,7 @@ def test_index(bcl2_first25):
     with open(f"{bcl2_first25}.hp.k24.scaled5.sig.zip.siglist") as f:
         assert f.readlines() == [f"{bcl2_first25}.hp.k24.scaled5.sig.zip"]
 
+    # TODO: Actually test this, e.g. with what Sourmash Branchwater does for testing indices
+    # https://github.com/sourmash-bio/sourmash_plugin_branchwater/blob/776727091d78af7de4990578ec6d07b12882c1b8/src/python/tests/test_index.py#L595
+    # https://github.com/seanome/kmerseek/issues/3
+    assert os.path.exists(f"{bcl2_first25}.hp.k24.scaled5.sig.zip.rocksdb")
