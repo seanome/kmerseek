f073a299048297cfe9fde98f9a14b258c94653e9
Olga Botvinnik <olga.botvinnik@gmail.com>
2025-02-26 17:17:01 -0800

    Read in the csv files and sort by match name to make sure we are comparing apples to apples

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,3 +1,6 @@
+from io import StringIO
+
+import polars as pl
 from click.testing import CliRunner
 
 from kmerseek.main import cli
@@ -19,16 +22,21 @@ def test_search(ced9, bcl2_first25, bcl2_hp_k16_sig_zip):
     )
     assert result.exit_code == 0
 
-    assert (
-        result.stdout
-        == """match_name,query_name,query_start,query_end,query,match_start,match_end,match
+    true_output = pl.read_csv(
+        StringIO(
+            """match_name,query_name,query_start,query_end,query,match_start,match_end,match
 sp|Q9UK96|FBX10_HUMAN F-box only protein 10 OS=Homo sapiens OX=9606 GN=FBXO10 PE=1 SV=3,sp|P41958|CED9_CAEEL Apoptosis regulator ced-9 OS=Caenorhabditis elegans OX=6239 GN=ced-9 PE=1 SV=1,59,92,MSIGESIDGKINDWEEPGIVGVVVCGRMMFSLK,57,90,PNWPNQPDVEPESWREAAGIYILYHGNPVVSGN
 sp|Q12982|BNIP2_HUMAN BCL2/adenovirus E1B 19 kDa protein-interacting protein 2 OS=Homo sapiens OX=9606 GN=BNIP2 PE=1 SV=1,sp|P41958|CED9_CAEEL Apoptosis regulator ced-9 OS=Caenorhabditis elegans OX=6239 GN=ced-9 PE=1 SV=1,76,108,RLDIEGFVVDYFTHRILFVYTSLFIKTRIRNN,23,55,SIEADILAITGPEDQPLLAVTRPFISSKFSQK
 "sp|Q9BXH1|BBC3_HUMAN Bcl-2-binding component 3, isoforms 1/2 OS=Homo sapiens OX=9606 GN=BBC3 PE=1 SV=1",sp|P41958|CED9_CAEEL Apoptosis regulator ced-9 OS=Caenorhabditis elegans OX=6239 GN=ced-9 PE=1 SV=1,170,187,LIGLISFGGFVAAKMME,46,63,APAAPTLLPAAYLCAPT
 sp|Q13625|ASPP2_HUMAN Apoptosis-stimulating of p53 protein 2 OS=Homo sapiens OX=9606 GN=TP53BP2 PE=1 SV=2,sp|P41958|CED9_CAEEL Apoptosis regulator ced-9 OS=Caenorhabditis elegans OX=6239 GN=ced-9 PE=1 SV=1,241,257,KVGRRKQNRRWSMIGA,1084,1100,TIIHREDEDEIEWWWA
 sp|Q16611|BAK_HUMAN Bcl-2 homologous antagonist/killer OS=Homo sapiens OX=9606 GN=BAK1 PE=1 SV=1,sp|P41958|CED9_CAEEL Apoptosis regulator ced-9 OS=Caenorhabditis elegans OX=6239 GN=ced-9 PE=1 SV=1,245,261,RKQNRRWSMIGAGVTA,42,58,HQQEQEAEGVAAPADP
 """
-    )
+        )
+    ).sort_values("match_name")
+    test_output = pl.read_csv(result.stdout).sort_values("match_name")
+
+    assert test_output.equals(true_output)
+
     assert (
         result.stderr
         == """
