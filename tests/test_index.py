9ffb40efe6ed212e1fa89ca2815ac4c14b783502
Olga Botvinnik <olga.botvinnik@gmail.com>
2025-04-03 11:27:07 -0700

    Add separate test for when doing k-mer extraction

--- tests/test_index.py
+++ tests/test_index.py
@@ -24,6 +24,35 @@ def test_index(bcl2_first25):
     # https://github.com/seanome/kmerseek/issues/3
     assert os.path.exists(f"{bcl2_first25}.hp.k24.scaled5.sig.zip")
 
+
+    assert os.path.exists(f"{bcl2_first25}.hp.k24.scaled5.sig.zip.siglist")
+    with open(f"{bcl2_first25}.hp.k24.scaled5.sig.zip.siglist") as f:
+        assert f.readlines() == [f"{bcl2_first25}.hp.k24.scaled5.sig.zip"]
+
+    # TODO: Actually test this, e.g. with what Sourmash Branchwater does for testing indices
+    # https://github.com/sourmash-bio/sourmash_plugin_branchwater/blob/776727091d78af7de4990578ec6d07b12882c1b8/src/python/tests/test_index.py#L595
+    # https://github.com/seanome/kmerseek/issues/3
+    assert os.path.exists(f"{bcl2_first25}.hp.k24.scaled5.sig.zip.rocksdb")
+
+
+def test_index_do_kmer_extraction(bcl2_first25):
+    runner = CliRunner()
+    result = runner.invoke(cli, ["index", "--do-kmer-extraction", "--force", bcl2_first25])
+    assert result.exit_code == 0
+
+    # Make sure all the files got created
+    assert os.path.exists(f"{bcl2_first25}.manysketch.csv")
+    with open(f"{bcl2_first25}.manysketch.csv") as f:
+        assert f.readlines() == [
+            "name,genome_filename,protein_filename\n",
+            f"{os.path.basename(bcl2_first25)},,{bcl2_first25}\n",
+        ]
+
+    # TODO: Actually test for the signature in this file by reading it in with
+    # `sourmash.load_file_as_signatures`
+    # https://github.com/seanome/kmerseek/issues/3
+    assert os.path.exists(f"{bcl2_first25}.hp.k24.scaled5.sig.zip")
+
     # TODO: Actually test this by reading it in with polars, then:
     # - assert the `.head()`
     # - assert the `.tail()`
