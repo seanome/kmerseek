b1c95fbc3053685e402c885b7ac90cdcb6a03a51
Olga Botvinnik <olga.botvinnik@gmail.com>
2025-06-17 14:15:12 -0700

    Add CQFFEE to PLANTANDANIMALGENQMES string for longer word testing and to get the other categories of dayhoff (for C cysteine and F tryptophan aromatic)

--- src/rust/tests/test_fixtures.rs
+++ src/rust/tests/test_fixtures.rs
@@ -2,7 +2,7 @@ pub const TEST_FASTA: &str =
     "tests/testdata/index/bcl2_first25_uniprotkb_accession_O43236_OR_accession_2025_02_06.fasta.gz";
 
 pub const TEST_KMER: &str = "LIVINGALIVE";
-pub const TEST_PROTEIN: &str = "PLANTANDANIMALGENQMES";
+pub const TEST_PROTEIN: &str = "PLANTYANDANIMALGENQMESCQFFEE";
 
 // "O" (pyrrolysine) is not a valid amino acid for our purposes
 pub const TEST_PROTEIN_INVALID: &str = "PLANTANDANIMALGENOMES";
