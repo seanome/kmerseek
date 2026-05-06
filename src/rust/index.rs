749d0477cba581cb1f5af5dea65c851751e0c1a2
Olga Botvinnik <olga.botvinnik@gmail.com>
2026-05-06 06:19:04 -0700

    Make sure HP alphabet is uppercased

--- src/rust/index.rs
+++ src/rust/index.rs
@@ -21,6 +21,7 @@ use crate::encoding::{
 };
 use crate::errors::{IndexError, IndexResult};
 use crate::hp_alphabets::HpAlphabet;
+use crate::hp_alphabets::HpAlphabet;
 use crate::signature::{SignatureAccess, SEED};
 use crate::sketch::{ProteinSketch, ProteinSketchStore};
 
--- src/rust/tests/mod.rs
+++ src/rust/tests/mod.rs
@@ -1,4 +1,5 @@
 mod test_cli;
+mod test_hp_encoding;
 pub mod test_fixtures;
 mod test_hp_encoding;
 pub mod test_utils;
